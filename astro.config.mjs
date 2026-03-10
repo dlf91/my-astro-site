554f16099d71fb4fce2c62e7ddf45e74441fb229
danushka_tryonics <danushka@tryonics.com>
2026-03-10 16:08:05 +0530

    path test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://dlf91.github.io',
-  base: '/my-astro-site', // This tells Astro to add /my-astro-site/ to all your links
+  base: '/my-astro-site/', // This tells Astro to add /my-astro-site/ to all your links
 });
