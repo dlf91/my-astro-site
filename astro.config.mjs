34bc2b9d2b7cc9cfec0f5ea44e5933b650cf5466
danushka_tryonics <danushka@tryonics.com>
2026-05-21 07:46:57 +0530

    chore: configured automated CI/CD pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://dlf91.github.io',
-  base: '/my-astro-site/', // This tells Astro to add /my-astro-site/ to all your links
+  site: 'https://vantagemobilemechanic.com.au',
+
+  base: '/', 
+  // 3. Optional: Keeps output structure clean for standard Apache servers
+  output: 'static',
+
+  //site: 'https://dlf91.github.io',
+  //base: '/my-astro-site/', // This tells Astro to add /my-astro-site/ to all your links
 });
